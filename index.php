d4445f4d4e3c397dd96a624f56b83f0c4a09ea39
BRUNO POSTALI <bruno.postali@gmail.com>
2022-01-21 21:21:55 -0300

    Basic Login SYstem in PHP

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+          <meta charset="UTF-8">
+          <link rel="shortcut icon" href="images/favicon.ico" type="image/x-ico">
+          <title>Simple login page in PHP</title>
+    </head>
+    <body>
+        <h1>Login</h1>
+    </body>
+</html>
